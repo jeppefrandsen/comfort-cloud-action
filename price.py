4a30e1eac7ddc500ea654795f73ac8e9ff451b85
Jeppe Frandsen <jeppe@algrens.dk>
2024-01-06 15:15:26 +0100

    Corrected linting

--- price.py
+++ price.py
@@ -4,17 +4,16 @@ from datetime import datetime
 
 PRICE_API_URL = "https://www.billigkwh.dk/api/Priser/HentPriser"
 
+
 def price(area: str, transport: str, company: str) -> float | None:
     now = datetime.now(pytz.timezone("Europe/Copenhagen"))
     hour = int(datetime.strftime(now, "%H"))
     params = {'sted': area, 'netselskab': transport, 'produkt': company}
     headers = {'User-Agent': 'UserAgent/1.0'}
 
-    response = requests.get(url=PRICE_API_URL,params=params, headers=headers).json()    
+    response = requests.get(url=PRICE_API_URL, params=params, headers=headers).json()
     if response:
         prices = response[0].get('priser', [])
         if prices:
             return float(prices[hour])
     return None
-
-print(price("DK1", "n1_c", "nettopower"))
